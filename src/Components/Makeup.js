33d25417df4ced1292217f6c8f6c6561146df5fd
disha2027 <dishabhat47@gmail.com>
2025-04-15 17:37:04 +0530

    Makeup.js file is added

---
+++ src/Components/Makeup.js
@@ -0,0 +1,39 @@
+import React from 'react'
+const Makeup = () => {
+  return (
+    <div>
+      <div className='container'>
+    <div className='row'>
+        <div className='col-lg-4'>
+            <h4 className='text-center'>Makeup</h4>
+         <table className='table table-bordered'>
+            <tbody>
+         <tr>
+              <td>Lipstick</td>
+         </tr>
+         <tr>
+            <td>Foundation</td>
+         </tr>
+         <tr>
+             <td>Bestseller</td>
+         </tr>
+         <tr>
+             <td>Eye Shadow</td>
+         </tr>
+         <tr>
+             <td>Concealer</td>
+         </tr>
+         <tr>
+             <td>Eyeliners&Kajal</td>
+         </tr>
+         </tbody>
+         </table>
+        </div>
+
+    </div>
+      </div>
+    </div>
+  )
+}
+
+export default Makeup
